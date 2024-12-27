e6899f45d839cfd9e9cea52fc77e2f166851b7e8
Roel U <roelul@outlook.com>
2024-12-27 12:02:06 -0700

    Add container for grid, generate grid now a funct.
    Generating a grid, is now in a function, so the
    user can call it with buttons that will be added
    further.
    
    The grid is now in a container, so the grid will
    always have the same size no mather the number of
    grids in rows or columns.

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
 
+const gridContainer = document.querySelector(".grid-container")
 
-for (let gridRow = 0; gridRow < 16; gridRow++) {
-    const row = document.createElement("div")
-    row.setAttribute("class","row")
-    document.body.appendChild(row)
-    for(let gridColumn = 0;gridColumn < 16; gridColumn++){
-        const newDiv = document.createElement("div")
-        newDiv.setAttribute("class","grid")
-        row.appendChild(newDiv)
+function generateGrid(){
+    for (let gridRow = 0; gridRow < 4; gridRow++) {
+        const row = document.createElement("div")
+        row.setAttribute("class","row")
+        gridContainer.appendChild(row)
+        for(let gridColumn = 0;gridColumn < 16; gridColumn++){
+            const newDiv = document.createElement("div")
+            newDiv.setAttribute("class","grid")
+            row.appendChild(newDiv)
+        }
     }
 }
 
