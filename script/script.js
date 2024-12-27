fab3be0658713ae96b0e1c2420c1a011eae239b4
Roel U <roelul@outlook.com>
2024-12-27 14:56:34 -0700

    Move grid event listener inside the grid generator
    Moved the event listener to correctly add the
    listener to the grid elements everytime they are
    generated.

--- script/script.js
+++ script/script.js
@@ -1,26 +1,27 @@
 
 const gridContainer = document.querySelector("#grid-container")
 
-function generateGrid(){
+function generateGrid(gridSize){
 
-    for (let gridRow = 0; gridRow < 4; gridRow++) {
+    for (let gridRow = 0; gridRow < gridSize; gridRow++) {
         const row = document.createElement("div")
         row.setAttribute("class","row")
         gridContainer.appendChild(row)
 
-        for(let gridColumn = 0;gridColumn < 16; gridColumn++){
+        for(let gridColumn = 0;gridColumn < gridSize; gridColumn++){
             const newDiv = document.createElement("div")
             newDiv.setAttribute("class","grid")
             row.appendChild(newDiv)
         }
+        const gridObjects = document.querySelectorAll(".grid")
+
+        gridObjects.forEach((grid)=>{
+            grid.addEventListener("mouseenter",()=>{
+                grid.style.backgroundColor = "black"
+            })
+        })
     }
 }
 
-const gridObjects = document.querySelectorAll(".grid")
 
-gridObjects.forEach((grid)=>{
-    grid.addEventListener("mouseenter",()=>{
-        grid.style.backgroundColor = "black"
-    })
-})
 
