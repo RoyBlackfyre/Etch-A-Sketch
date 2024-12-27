e853caf2cc0710b62b23beb9d82e8f17712b181f
Roel U <roelul@outlook.com>
2024-12-27 12:30:57 -0700

    Add container, button and style for elements
    To make the page in a flexbox layout, i added an
    additional div, to contain all the elements
    
    Added style to elements inside the container for
    esthetic.

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
 
-const gridContainer = document.querySelector(".grid-container")
+const gridContainer = document.querySelector("#grid-container")
 
 function generateGrid(){
+
     for (let gridRow = 0; gridRow < 4; gridRow++) {
         const row = document.createElement("div")
         row.setAttribute("class","row")
         gridContainer.appendChild(row)
+
         for(let gridColumn = 0;gridColumn < 16; gridColumn++){
             const newDiv = document.createElement("div")
             newDiv.setAttribute("class","grid")
