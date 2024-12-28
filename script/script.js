e2772fcf71caef21b6fa7cce8b2d043be8fc4924
Roel U <roelul@outlook.com>
2024-12-28 12:34:34 -0700

    Add functionality to fill grid with random colors
    The button of random colors now set each grid to
    fill with one of the 7 colors of the rainbows
    randomly.

--- script/script.js
+++ script/script.js
@@ -2,9 +2,11 @@
 const gridContainer = document.querySelector("#grid-container")
 const generateGridButton = document.querySelector("#button-grid-generator")
 const blackButton = document.querySelector("#button-black-fill")
+const rainbowButton = document.querySelector("#button-rainbow-fill")
+
 
 document.body.addEventListener("click",(e)=>{
-    if(e.target.id === "button-grid-generator"){
+    if(e.target === generateGridButton){
         const promptInput = prompt("Enter size of grid from 1 to 100")
         const gridSize = parseInt(promptInput)
         if(0<gridSize && gridSize<=100){
@@ -15,8 +17,12 @@ document.body.addEventListener("click",(e)=>{
         }
     }
 
-    if(e.target.id === "button-black-fill"){
+    if(e.target === blackButton){
         fillBlack()
+    }
+
+    if(e.target.closest("#button-rainbow-fill")){
+        fillRainbow()
     }
 })
 
@@ -44,5 +50,26 @@ function fillBlack(){
                 })
             })
 }
+
+function fillRainbow(){
+
+    const gridObjects = document.querySelectorAll(".grid")
+        
+    gridObjects.forEach((grid)=>{
+            
+            grid.addEventListener("mouseenter",()=>{
+                grid.style.backgroundColor = generateRandomColor()
+            })
+        })
+
+}
+
+function generateRandomColor(){
+    const rainbowColors = ["red","orange","yellow","green","blue","indigo","violet"]
+    let randomNumber = Math.floor(((Math.random()) * rainbowColors.length))
+    return rainbowColors[randomNumber]
+}
+
+
 
 
